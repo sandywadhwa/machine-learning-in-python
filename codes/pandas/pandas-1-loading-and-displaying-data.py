cc6867dfe4960e2b2e39aaaf8d7add34d42851b9
Sandeep <Sandeep>
2017-12-01 20:16:49 +0530

    Pandas Getting Started First Example Code Done

---
+++ codes/pandas/pandas-1-loading-and-displaying-data.py
@@ -0,0 +1,19 @@
+import pandas
+import numpy
+import matplotlib
+
+# READ CSV FILE 
+df = pandas.read_csv("../../data/uci-pima-indian-diabetes-data-original.csv")
+
+# Print Dimensions of Data (Rows, Cols)
+print("Dimensions of Data", df.shape)
+
+# Print First n Rows
+n = 5
+print(df.head(n))
+
+# Print Last n Rows
+print(df.tail(n))
+
+# Get One Particular Cell Value
+# print(df[0]["diab_pred"])
